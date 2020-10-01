e9e80a843daf96fe7f7d5f3ef0154f00f6b4a4e7
Kostiantyn Zhereb <zhereb@gmail.com>
2020-10-01 08:05:25 +0300

    Solving quadratic equations

--- code-examples/lecture1_intro/solve_equations.py
+++ code-examples/lecture1_intro/solve_equations.py
@@ -1,7 +1,22 @@
+import math
+
 
 """
 Solves linear equation a * x + b == 0
 """
 def solve_linear(a,b):
   return -b/a
+
+
+"""
+Solves quadratic equation a * x^2 + b * x + c == 0
+"""  
+def solve_quadratic(a,b,c):
+  D = b*b - 4 * a * c
+  sqrt_D = math.sqrt(D)
+  x1 = (-b + sqrt_D) / (2 * a)
+  x2 = (-b - sqrt_D) / (2 * a)
+  return (x1,x2)
+  
+
   
