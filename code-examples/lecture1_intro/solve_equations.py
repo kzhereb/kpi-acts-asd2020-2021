95480cacef8516e5f55d9cd46facaa9309756809
Kostiantyn Zhereb <zhereb@gmail.com>
2020-10-01 08:18:35 +0300

    Solving equations using approximate numeric methods

--- code-examples/lecture1_intro/solve_equations.py
+++ code-examples/lecture1_intro/solve_equations.py
@@ -20,5 +20,15 @@ def solve_quadratic(a,b,c):
   x2 = (-b - sqrt_D) / (2 * a)
   return (x1,x2)
   
+"""
+Approximate solution of quadratic equation a * x^2 + b * x + c == 0
+"""
 
+def solve_approximate(a,b,c,steps = 100):
+  x = 0
+  for i in range(steps):
+    f_x = a * x * x + b * x + c 
+    df_x = 2 * x + b 
+    x = x - f_x/df_x
+  return x
   
